233a60a9a56d7d0b71122d6e805ad1bf3554a2a2
George Shevtsov <georgiy.shevtsov@gmail.com>
2023-04-14 16:36:39 +0300

    add image loading to avoid showing favourite icon before image was loaded

--- src/components/ImagesGalleryItem.tsx
+++ src/components/ImagesGalleryItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
@@ -18,17 +18,24 @@ export function ImagesGalleryItem({
   isInFavourites,
   handleChangeFavourites,
 }: ImagesGalleryItemProps) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <ImageListItem>
-      <img src={url} alt={alt} loading="lazy"></img>
+      <img src={url} alt={alt} loading="lazy" onLoad={() => setImageLoaded(true)}></img>
       <ImageListItemBar
         sx={{ background: 'rgba(0,0,0, 0)' }}
         position="bottom"
         actionPosition="left"
         actionIcon={
-          <IconButton onClick={handleChangeFavourites}>
-            {!isInFavourites ? <StarBorderIcon htmlColor="gold" /> : <StarIcon htmlColor="gold" />}
-          </IconButton>
+          imageLoaded && (
+            <IconButton onClick={handleChangeFavourites}>
+              {!isInFavourites ? (
+                <StarBorderIcon htmlColor="gold" />
+              ) : (
+                <StarIcon htmlColor="gold" />
+              )}
+            </IconButton>
+          )
         }
       />
     </ImageListItem>
